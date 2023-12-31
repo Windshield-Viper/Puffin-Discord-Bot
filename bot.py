a2909fd2482556946fb7dd7bc3f4524e95cfc0c0
Siddharth Gupta <emailsiddha@gmail.com>
2023-12-31 16:35:18 -0500

    overhaul bot so configure command actually works

--- bot.py
+++ bot.py
@@ -1,137 +1,94 @@
 import discord
-import responses
 import json
 from dotenv import load_dotenv
 import os
 from discord.ext import commands
+from model import pos_neg_neu_model, emotion_model
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-
-TOKEN = 'MTE4NTA3NjEyNTc1MjM2NTEzNg.GdiEPo.qR5aiYEDXl6maS2dSu3H_b76IVgstgAz4jjjg8'
 GUILDS = "Scifair Bot Server"
 
-config_bot = commands.Bot(command_prefix='!sent_bot configure')
+intents = discord.Intents.default()
+intents.message_content = True
 
+sent_bot = commands.Bot(command_prefix='!sent_bot ', intents=intents)
+@sent_bot.event
+async def on_ready():
+    print(f'Logged in as {sent_bot.user.name} ({sent_bot.user.id})')
+    print('------')
 
-@config_bot.command()
-async def get_server_name(ctx):
-    await ctx.send("What is the name of your server?")
-
-    @ctx.event
-    async def on_message(message):
-        if message.author == ctx.user:
-            return
-
-        server_name = str(message.content).strip().upper()
-
-        print(f'{username} said: {user_message} ({channel})')
-
-        if message.content[-3:] == " -p":
-            user_message = user_message[0:len(user_message) - 3]
-            await send_message(message, user_message, is_private=True)
-        elif message.content == "!sent_bot configure":
-            # await configure(message)
-            return
-        else:
-            await send_message(message, user_message, is_private=False)
-
-async def main_configure(ctx):
-    try:
-        # if user_message == "!sent_bot configure":
-        #     await configure(message)
-        # else:
-        await ctx.send("Configuration started.")
-
-    except Exception as e:
-        print(e)
-
-
-
-bot.run(TOKEN)
-
-
-# async def configure(message):
-#
-#     intents = discord.Intents.default()
-#     client = discord.Client(intents=intents)
-#     # configure name and description in JSON file config.json
-#     await message.author.send("Configuration started. ")
-#     await message.author.send("What is the name of your server?")
-#
-#     @client.event
-#     async def on_message(message):
-#         server_name = message.content
-#         await message.author.send("Your server name is: " + str(server_name))
-#         await message.author.send("What is the description of your server?")
-#
-#     # @client.event
-#     # async def on_message(message):
-#     #     server_description = message.content
-#     #
-#     #
-#     # # name = await client.wait_for('message')
-#     # # await message.author.send("What is the description of your server?")
-#     # # description = await client.wait_for('message')
-#     # # add name and description to JSON file, keeping other contents of the file
-#     # with open('config.json', 'r') as f:
-#     #     config = json.load(f)
-#     # config['name'] = server_name
-#     # config['description'] = server_description
-#     # with open('config.json', 'w') as f:
-#     #     json.dump(config, f)
-#     # await message.author.send("Configuration complete. ")
-#
-
-async def send_message(message, user_message, is_private):
-    try:
-        # if user_message == "!sent_bot configure":
-        #     await configure(message)
-        # else:
-            response = responses.get_response(user_message)
-            await message.author.send(response) if is_private else await message.channel.send(response)
-
-    except Exception as e:
-        print(e)
-
-def run_discord_bot():
-    TOKEN = 'MTE4NTA3NjEyNTc1MjM2NTEzNg.GdiEPo.qR5aiYEDXl6maS2dSu3H_b76IVgstgAz4jjjg8'
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
-
-    @client.event
-    async def on_ready():
-        print(f'{client.user} has connected to Discord!')
-        for guild in client.guilds:
+    for guild in sent_bot.guilds:
             if guild.name == GUILD:
                 break
 
-        print(
-            f'{client.user} is connected to the following guild:\n'
-            f'{guild.name}(id: {guild.id})'
-        )
+            print(
+                f'{sent_bot.user} is connected to the following guild:\n'
+                f'{guild.name}(id: {guild.id})'
+            )
+@sent_bot.command()
+async def analyze(ctx, *, p_message):
+    # whether the message is positive, negative, or neutral
+    base_message = pos_neg_neu_model(p_message[18:])
+    label = base_message[0]["label"]
+    score = base_message[0]["score"]
+   # emotion of the message
+    emotion_message = emotion_model(p_message[18:])
+    emotion_label = emotion_message[0]["label"]
+    emotion_score = emotion_message[0]["score"]
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
 
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
+    response = f"I am {round(score * 100)}% sure that was a `{label}` message. I am {round(emotion_score * 100)}% sure that was a message of emotion `{emotion_label}`."
+    await ctx.send(response)
+@sent_bot.command()
+async def hello(ctx):
+    await ctx.send("Hello from sent_bot!")
 
-        print(f'{username} said: {user_message} ({channel})')
+@sent_bot.command()
+async def bot_help(ctx):
+    await ctx.send("`Available commands: !sent_bot hello, !sent_bot analyze <message> "
+                "(e.g. !sent_bot analyze I love you), !sent_bot help`.")
 
-        if message.content[-3:] == " -p":
-            user_message = user_message[0:len(user_message) - 3]
-            await send_message(message, user_message, is_private=True)
-        elif message.content == "!sent_bot configure":
-            #await configure(message)
-            return
-        else:
-            await send_message(message, user_message, is_private=False)
+@sent_bot.command()
+async def configure(ctx):
 
-    client.run(TOKEN)
+    try:
+        author = ctx.message.author
+
+        await author.send(f"Please provide the server name")
+        server_name = await sent_bot.wait_for("message", check=lambda m: m.author == ctx.author, timeout=60)
+        server_name = str(server_name.content).strip().upper()
+
+        await author.send(f"Please provide the server description")
+        server_description = await sent_bot.wait_for("message", check=lambda m: m.author == ctx.author, timeout=60)
+        server_description = str(server_description.content).strip().upper()
+
+        # Store configuration in a JSON file
+        config = {
+            "server_name": server_name,
+            "server_description": server_description
+        }
+
+        with open("config.json", "w") as f:
+            json.dump(config, f)
+
+
+
+        await author.send("Configuration saved successfully.")
+
+    except Exception as err:
+        await author.send(f"Configuration failed: {err}")
+
+@sent_bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send(f"Unknown command. Type `!help` for a list of available commands.")
+
+
+
+def run_sent_bot():
+    sent_bot.run(TOKEN)
+
+
--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import bot
 
 if __name__ == '__main__':
     # run the bot
-    bot.run_discord_bot()
+    #bot.run_discord_bot()
+    # run the configuration
+    bot.run_sent_bot()
